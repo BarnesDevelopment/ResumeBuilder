a7e14c596caf186ec86b7fdee72b720ff944fb70
sb17057 <sbarnes@vermeer.com>
2023-07-31 23:13:42 -0500

    added swagger generation

--- ResumeAPI/ResumeAPI/Startup.cs
+++ ResumeAPI/ResumeAPI/Startup.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+using Microsoft.OpenApi.Models;
 using ResumeAPI.Database;
 using ResumeAPI.Orchestrator;
 using ResumeAPI.Services;
@@ -13,13 +15,31 @@ public class Startup
         configRoot = configuration;
     }
     public void ConfigureServices(IServiceCollection services) {
+        services.AddControllers();
         services.AddEndpointsApiExplorer();
-        services.AddSwaggerGen();
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "ResumeAPI", 
+                Version = "v1",
+            });
+            //c.DocumentFilter<HealthChecksFilter>();
+            
+            var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+            c.IncludeXmlComments(xmlPath);
+        });
 
         services.AddTransient<IResumeOrchestrator, ResumeOrchestrator>();
         services.AddTransient<IResumeService, ResumeService>();
         
         services.AddTransient<IMySqlContext, MySqlContext>();
+        
+        services.AddHttpClient();
+        
+        // services.AddHealthChecks()
+        //     .AddOracle(appSettings.ConnectionStrings.Glovia, "select * from v$version", "Glovia Db");
         
     #if DEBUG
         services.AddSassCompiler();
@@ -29,15 +49,33 @@ public class Startup
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
-            app.UseSwagger();
-            app.UseSwaggerUI();
+            app.UseDeveloperExceptionPage();
         }
+        
+        app.UseSwagger();
+        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ResumeAPI v1"));
+        app.UseStaticFiles();
+        app.UseRouting();
 
         app.UseHttpsRedirection();
 
         app.UseAuthorization();
 
-        app.MapControllers();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+            // endpoints.MapHealthChecks("/healthcheck", new HealthCheckOptions
+            // {
+            //     AllowCachingResponses = false,
+            //     ResultStatusCodes =
+            //     {
+            //         [HealthStatus.Healthy] = StatusCodes.Status200OK,
+            //         [HealthStatus.Degraded] = StatusCodes.Status200OK,
+            //         [HealthStatus.Unhealthy] = StatusCodes.Status503ServiceUnavailable
+            //     },
+            //     ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            // });
+        });
 
         app.Run();
     }
--- makefile
+++ makefile
@@ -7,3 +7,12 @@ docker-run:
 docker-build-and-run:
 	docker build -t sambobbarnes/resume-api .
 	docker run -p 8080:80/tcp -p 10443:443/tcp sambobbarnes/resume-api
+
+generate-swagger:
+	cd ./ResumeAPI/ResumeAPI && \
+	dotnet tool restore && \
+	dotnet build --configuration Release -o out && \
+	cd out && \
+	dotnet swagger tofile --output ../../Schemas/swagger.json ResumeAPI.dll v1 && \
+	cd .. && \
+	rmdir out /s /q
