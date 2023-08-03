7cac315883cc37b639b03db8cc66bca9b289377d
sb17057 <sbarnes@vermeer.com>
2023-08-03 08:11:46 -0500

    header base

--- App/resume-builder/src/app/app.module.ts
+++ App/resume-builder/src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { LoginModule } from './login/login.module';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { CommonModule } from './common/common.module';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, PageNotFoundComponent],
-  imports: [BrowserModule, AppRoutingModule, LoginModule],
+  imports: [BrowserModule, AppRoutingModule, LoginModule, CommonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
