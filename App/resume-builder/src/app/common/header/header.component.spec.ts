34d4f40eb90a1ca18d06e90fc9b69ebdd82aa7b8
SamBobBarnes <samuelrbarnes@outlook.com>
2023-08-03 22:40:10 -0500

    tested panel

--- App/resume-builder/src/app/common/header/header.component.spec.ts
+++ App/resume-builder/src/app/common/header/header.component.spec.ts
@@ -1,7 +1,7 @@
 import { HeaderComponent } from './header.component';
 import {renderRootComponent} from "../RenderRootComponent";
 import {User} from "../../models/User";
-import {screen} from "@testing-library/angular";
+import {fireEvent, screen} from "@testing-library/angular";
 
 describe('HeaderComponent', () => {
 
@@ -14,34 +14,52 @@ describe('HeaderComponent', () => {
     username: ''
   };
 
-  beforeEach(() => {
-
-  });
-
   it('should display username if user is not null', async () => {
     await render(user);
     expect(screen.getByText('full name')).toBeTruthy();
   });
 
-  it('should display login button if user is null', async () => {
-    await render();
-    const button = screen.getByTitle('login');
-    expect(button).toBeTruthy();
+  describe('Login Button', () => {
+    it('should display login button if user is null', async () => {
+      await render();
+      const button = screen.getByTitle('login');
+      expect(button).toBeTruthy();
+    });
+
+    it('should have correct login href', async () => {
+      await render();
+      const button = screen.getByTitle('login');
+      expect(button.getAttribute("href")).toBe('/login');
+    });
+
+    it('should have correct login button text', async () => {
+      await render();
+      const button = screen.getByTitle('login');
+      expect(button.getAttribute("text")).toBe('Login');
+    });
   });
 
-  it('should have correct login href', async () => {
-    await render();
-    const button = screen.getByTitle('login');
-    expect(button.getAttribute("href")).toBe('/login');
+  describe('User Panel', () => {
+    it('should show user panel when toggle is clicked', async () => {
+      await render(user);
+      const userButton = screen.getByText('full name');
+      fireEvent.click(userButton);
+      expect(screen.getByTitle('userPanel')).toBeTruthy();
+    });
+
+    it('should hide user panel when toggle is clicked again', async () => {
+      await render(user);
+      const userButton = screen.getByText('full name');
+      fireEvent.click(userButton);
+      fireEvent.click(userButton);
+      expect(screen.queryByTitle('userPanel')).toBeFalsy();
+    });
+
+    it('should hide user panel by default', async () => {
+      await render(user);
+      expect(screen.queryByTitle('userPanel')).toBeFalsy();
+    });
   });
-
-  it('should have correct login button text', async () => {
-    await render();
-    const button = screen.getByTitle('login');
-    expect(button.getAttribute("text")).toBe('Login');
-  });
-
-
 });
 
 const render = async (user: User = null) => {
