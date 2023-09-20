11fc271f09ea5a65d026dc2736950e09742761e6
sb17057 <sbarnes@vermeer.com>
2023-09-20 11:46:57 -0500

    section types

---
+++ ResumeAPI/ResumeAPI/Models/ResumeSectionType.cs
@@ -0,0 +1,14 @@
+namespace ResumeAPI.Models;
+
+public static class ResumeSectionType
+{
+    public const string Resume = "Resume";
+    public const string Section = "Section";
+    public const string Separator = "Separator";
+    public const string Paragraph = "Paragraph";
+    public const string Title = "Title";
+    public const string Line = "Line";
+    public const string List = "List";
+    public const string ListItem = "ListItem";
+    public const string SectionItem = "SectionItem";
+}
