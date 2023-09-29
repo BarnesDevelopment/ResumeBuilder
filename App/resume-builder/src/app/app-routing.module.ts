7a4a4e2db3c49b42b5aa8953d6bccaf12c5befea
sb17057 <sbarnes@vermeer.com>
2023-09-29 14:32:51 -0500

    create page init

--- App/resume-builder/src/app/app-routing.module.ts
+++ App/resume-builder/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './login/components/login-page/login-page.component';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { CreateResumeComponent } from './resume/components/create-resume/create-resume.component';
 
 const routes: Routes = [
   {
@@ -13,6 +14,10 @@ const routes: Routes = [
     path: 'login',
     component: LoginPageComponent,
     runGuardsAndResolvers: 'always',
+  },
+  {
+    path: 'create',
+    component: CreateResumeComponent,
   },
   { path: '**', component: PageNotFoundComponent },
 ];
