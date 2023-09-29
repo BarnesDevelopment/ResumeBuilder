8c888e5f2f74e668ba3d433610325739ced0787f
sb17057 <sbarnes@vermeer.com>
2023-09-29 14:38:29 -0500

    create endpoint

--- App/resume-builder/src/app/models/Resume.ts
+++ App/resume-builder/src/app/models/Resume.ts
@@ -5,3 +5,28 @@ export interface ResumeHeader {
   comments: string;
   id: Guid;
 }
+
+export interface ResumeTreeNode {
+  children: ResumeTreeNode[];
+  comments: string;
+  id: Guid;
+  active: boolean;
+  userId: Guid;
+  parentId: Guid | null;
+  content: string;
+  sectionType: SectionType;
+  depth: number;
+  order: number;
+}
+
+export enum SectionType {
+  Resume = 'Resume',
+  Section = 'Section',
+  Separator = 'Separator',
+  Paragraph = 'Paragraph',
+  Title = 'Title',
+  Line = 'Line',
+  List = 'List',
+  ListItem = 'ListItem',
+  SectionItem = 'SectionItem',
+}
--- App/resume-builder/src/app/resume/services/resume.service.ts
+++ App/resume-builder/src/app/resume/services/resume.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { ResumeHeader } from '../../models/Resume';
+import { ResumeHeader, ResumeTreeNode } from '../../models/Resume';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environment/environment';
 import { LoginService } from '../../login/services/login.service';
@@ -21,6 +21,18 @@ export class ResumeService {
     const headers = new HttpHeaders().set('Authorization', cookie.cookie);
     return this.http.get<ResumeHeader[]>(
       `${this.env.apiBasePath}/resume/get-all`,
+      {
+        headers,
+      },
+    );
+  }
+
+  public createResume(resume: ResumeTreeNode): Observable<ResumeTreeNode> {
+    const cookie = this.loginService.getCookie();
+    const headers = new HttpHeaders().set('Authorization', cookie.cookie);
+    return this.http.post<ResumeTreeNode>(
+      `${this.env.apiBasePath}/resume/create`,
+      resume,
       {
         headers,
       },
