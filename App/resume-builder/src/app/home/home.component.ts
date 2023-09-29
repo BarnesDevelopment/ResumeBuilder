b49840cd4b46074078a81e22062627a486755065
sb17057 <sbarnes@vermeer.com>
2023-09-29 09:41:31 -0500

    basic resume cards

--- App/resume-builder/src/app/home/home.component.ts
+++ App/resume-builder/src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { ButtonStyle } from '../common/button/button.component';
+import { BorderStyle, ButtonStyle } from '../common/button/button.component';
 import { environment } from '../../environment/environment';
+import { ResumeHeader } from '../models/Resume';
+import { Guid } from 'guid-typescript';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -9,4 +11,22 @@ import { environment } from '../../environment/environment';
 export class HomeComponent {
   protected readonly ButtonStyle = ButtonStyle;
   protected readonly environment = environment;
+  resumes: ResumeHeader[] = [
+    {
+      title: 'Resume 1',
+      comments: 'This is my first resume',
+      id: Guid.create(),
+    },
+    {
+      title: 'Resume 2',
+      comments: 'This is my second resume',
+      id: Guid.create(),
+    },
+    {
+      title: 'Resume 3',
+      comments: 'This is my third resume',
+      id: Guid.create(),
+    },
+  ];
+  protected readonly BorderStyle = BorderStyle;
 }
