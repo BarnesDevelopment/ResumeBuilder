0ee0b157ce93f0165800b25ff417b246c8487c17
SamBobBarnes <samuelrbarnes@outlook.com>
2023-09-30 15:31:44 -0500

    fixed styling

--- App/resume-builder/src/app/resume/components/create-resume/create-resume.component.ts
+++ App/resume-builder/src/app/resume/components/create-resume/create-resume.component.ts
@@ -3,7 +3,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ResumeService } from '../../services/resume.service';
 import { ResumeTreeNode, SectionType } from '../../../models/Resume';
 import { Guid } from 'guid-typescript';
-import {ActivatedRoute, Router} from "@angular/router";
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-resume',
@@ -11,23 +11,24 @@ import {ActivatedRoute, Router} from "@angular/router";
   styleUrls: ['./create-resume.component.scss'],
 })
 export class CreateResumeComponent {
-  form: FormGroup = new FormGroup(
-    {
-      title: new FormControl('', [
-        Validators.required,
-        Validators.maxLength(150),
-        Validators.minLength(3),
-      ]),
-      comments: new FormControl('', [Validators.maxLength(500)]),
-    }
-  );
+  form: FormGroup = new FormGroup({
+    title: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(150),
+      Validators.minLength(3),
+    ]),
+    comments: new FormControl('', [Validators.maxLength(500)]),
+  });
 
-  constructor(private service: ResumeService, private router: Router, private route: ActivatedRoute) {}
+  constructor(
+    private service: ResumeService,
+    private router: Router,
+    private route: ActivatedRoute,
+  ) {}
 
   onSubmit() {
     console.log(this.form.value);
-    this.route.queryParams.subscribe(params => {
-
+    this.route.queryParams.subscribe((params) => {
       const resume: ResumeTreeNode = {
         content: this.form.controls['title'].value,
         comments: this.form.controls['comments'].value,
@@ -45,6 +46,5 @@ export class CreateResumeComponent {
         this.router.navigate(['/edit', res.id]);
       });
     });
-
   }
 }
