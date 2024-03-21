4892b0e59544a4a3ea9400601456736e63a93802
SamBobBarnes <samuelrbarnes@outlook.com>
2024-03-21 09:37:52 -0600

    refactored some tests

--- ResumeAPI/UnitTests/Helpers/CookieValidatorTests.cs
+++ ResumeAPI/UnitTests/Helpers/CookieValidatorTests.cs
@@ -1,56 +1,13 @@
-using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Threading.Tasks;
 using Moq;
 using ResumeAPI.Helpers;
 using ResumeAPI.Services;
 
-namespace UnitTests;
+namespace UnitTests.Helpers;
 
 public class HelperTests
 {
-    [Fact]
-    public void TagHelper_Write_CreateCorrectString()
-    {
-        var expected = @"<div class=""className""></div>";
-        var div = new TagBuilder("div");
-        div.AddCssClass("className");
-
-        var actual = div.Write();
-
-        actual.Should().Be(expected);
-    }
-
-    [Fact]
-    public void TagHelper_CreateTag_SetTagName()
-    {
-        var expected = @"<div></div>";
-        
-        var actual = TagHelper.CreatTag("div");
-
-        actual.TagName.Should().Be("div");
-        actual.Write().Should().Be(expected);
-    }
-
-    [Fact]
-    public void TagHelper_CreateTag_SetClassName()
-    {
-        var expected = @"<div class=""some-class-name""></div>";
-        
-        var actual = TagHelper.CreatTag("div", "some-class-name");
-
-        actual.Write().Should().Be(expected);
-    }
-
-    [Fact]
-    public void TagHelper_CreateTag_SetInnerText()
-    {
-        var expected = @"<span>some inner text</span>";
-        var someInnerText = "some inner text";
-        
-        var actual = TagHelper.CreatTag("span", "", someInnerText);
-
-        actual.Write().Should().Be(expected);
-    }
-    
     [Fact]
     public async Task CookieValidator_Validate_NoUser()
     {
