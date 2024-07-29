453a518f3ae321aa18c2c0ad725a99b372bd289e
sb17057 <sbarnes@vermeer.com>
2024-07-29 15:43:39 -0500

    paragraph init

---
+++ App/resume-builder/src/app/resume/components/edit-resume/components/section-paragraph/section-paragraph.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section-paragraph',
+  standalone: true,
+  imports: [],
+  templateUrl: './section-paragraph.component.html',
+  styleUrl: './section-paragraph.component.scss'
+})
+export class SectionParagraphComponent {
+
+}
