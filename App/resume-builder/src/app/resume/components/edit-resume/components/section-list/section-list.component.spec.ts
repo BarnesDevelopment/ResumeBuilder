2f6b1fcebe32e8cb4a4120773e0e9de8c6337667
sb17057 <sbarnes@vermeer.com>
2024-07-29 15:44:09 -0500

    list init and tests init

---
+++ App/resume-builder/src/app/resume/components/edit-resume/components/section-list/section-list.component.spec.ts
@@ -0,0 +1,18 @@
+describe('SectionListComponent', () => {
+  describe('Init', () => {
+    it('should show a single empty input', async () => {});
+    it('should show a plus button', async () => {});
+    it('should not show a minus button', async () => {});
+  });
+  describe('Enter Information', () => {
+    it('should create a new input when the plus button is clicked', async () => {});
+    it('should only show the plus on the last row', async () => {});
+    it('should show a minus button on each row when there are two or more inputs', async () => {});
+    it('should remove the current input when the minus button is clicked', async () => {});
+  });
+  describe('Load Data', () => {
+    it('should load the existing data into as many rows as necessary', async () => {});
+    it('should populate the form with text', async () => {});
+  });
+  describe('Save', () => {});
+});
