44a71a0c45d2b5202591a0bd4cd3f6bc551d871c
sb17057 <sbarnes@vermeer.com>
2024-07-31 13:36:14 -0500

    testing imports barrel file

---
+++ App/resume-builder/src/app/common/testing-imports.ts
@@ -0,0 +1,7 @@
+import '@testing-library/jest-dom';
+import { screen } from '@testing-library/angular';
+import { renderRootComponent } from './RenderRootComponent';
+import { NodeType, ResumeTreeNode } from '../models/Resume';
+import { Guid } from 'guid-typescript';
+
+export { screen, renderRootComponent, NodeType, ResumeTreeNode, Guid };
