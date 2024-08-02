cfda5e0db051233400dc46b1029c07b59799ed5b
sb17057 <sbarnes@vermeer.com>
2024-08-02 08:56:09 -0500

    paragraph

--- App/resume-builder/src/app/resume/components/edit-resume/components/section-paragraph/section-paragraph.component.spec.ts
+++ App/resume-builder/src/app/resume/components/edit-resume/components/section-paragraph/section-paragraph.component.spec.ts
@@ -1,23 +1,63 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {
+  fireEvent,
+  Guid,
+  NodeType,
+  renderRootComponent,
+  ResumeTreeNode,
+  screen,
+} from '../../../../../common/testing-imports';
 import { SectionParagraphComponent } from './section-paragraph.component';
+import { newResumeTreeNode } from '../../../../../models/Resume';
 
 describe('SectionParagraphComponent', () => {
-  let component: SectionParagraphComponent;
-  let fixture: ComponentFixture<SectionParagraphComponent>;
+  let node: ResumeTreeNode;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [SectionParagraphComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(SectionParagraphComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  beforeEach(() => {
+    node = {
+      nodeType: NodeType.Paragraph,
+      children: [],
+      content: '',
+      comments: '',
+      id: Guid.create(),
+      depth: 2,
+      parentId: Guid.create(),
+      active: true,
+      userId: Guid.create(),
+      order: 0,
+    };
   });
+  describe('Init', () => {
+    it('should show a single empty input', async () => {
+      const component = await render(node);
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+      expect(screen.queryByRole('textbox')).toBeInTheDocument();
+    });
   });
+  describe('Enter Information', () => {
+    it('should update the node content when the input is changed', async () => {
+      const component = await render(node);
+
+      const input = component.queryByRole('textbox');
+      fireEvent.input(input, { target: { value: 'Test' } });
+      component.detectChanges();
+      expect(component.fixture.componentInstance.node.content).toBe('Test');
+    });
+  });
+  describe('Load Data', () => {
+    it('should show the node content in the input', async () => {
+      node.content = 'Test';
+      const component = await render(node);
+
+      const input = screen.getByRole('textbox') as HTMLInputElement;
+
+      expect(input.value).toBe('Test');
+    });
+  });
+  describe('Save', () => {});
 });
+
+const render = async (node: ResumeTreeNode) => {
+  return await renderRootComponent(SectionParagraphComponent, {
+    componentProperties: { node },
+  });
+};
--- App/resume-builder/src/app/resume/components/edit-resume/components/section-paragraph/section-paragraph.component.ts
+++ App/resume-builder/src/app/resume/components/edit-resume/components/section-paragraph/section-paragraph.component.ts
@@ -1,13 +1,27 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ResumeTreeNode } from '../../../../../models/Resume';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-section-paragraph',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './section-paragraph.component.html',
   styleUrl: './section-paragraph.component.scss',
 })
-export class SectionParagraphComponent {
+export class SectionParagraphComponent implements OnInit {
   @Input() node: ResumeTreeNode;
+
+  form: FormGroup;
+
+  ngOnInit() {
+    console.log({ node: this.node });
+    this.form = new FormGroup({
+      paragraph: new FormControl(this.node.content),
+    });
+
+    this.form.valueChanges.subscribe(value => {
+      this.node.content = value.paragraph;
+    });
+  }
 }
