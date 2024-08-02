fba3de08449dcb1dfd639100018a9b10f9539611
sb17057 <sbarnes@vermeer.com>
2024-08-02 09:14:10 -0500

    edit tests init

--- App/resume-builder/src/app/resume/components/edit-resume/edit-resume.component.spec.ts
+++ App/resume-builder/src/app/resume/components/edit-resume/edit-resume.component.spec.ts
@@ -1,21 +1,74 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {
+  Guid,
+  NodeType,
+  renderRootComponent,
+  ResumeTreeNode,
+} from '../../../common/testing-imports';
 import { EditResumeComponent } from './edit-resume.component';
 
 describe('EditResumeComponent', () => {
-  let component: EditResumeComponent;
-  let fixture: ComponentFixture<EditResumeComponent>;
+  let rootNode: ResumeTreeNode;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [EditResumeComponent]
-    });
-    fixture = TestBed.createComponent(EditResumeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    rootNode = {
+      id: Guid.create(),
+      nodeType: NodeType.Resume,
+      userId: Guid.create(),
+      parentId: null,
+      content: 'Resume Name 1',
+      active: true,
+      order: 0,
+      depth: 0,
+      children: [],
+      comments: '',
+    };
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('Init', () => {
+    //load title and comments
+    //load name email phone
+    describe('Website', () => {
+      //load website
+      //add website button
+    });
+    describe('Sections', () => {
+      //add section button
+    });
+  });
+  describe('Load Data', () => {
+    describe('Website', () => {
+      //load website
+    });
+    describe('Sections', () => {
+      //load sections
+    });
+  });
+  describe('Edit', () => {
+    describe('Title', () => {
+      //edit title
+      //edit comments
+    });
+    describe('Personal Info', () => {
+      //edit name
+      //edit email
+      //edit phone
+    });
+    describe('Website', () => {
+      //add website
+      //remove website
+      //edit website
+    });
+    describe('Sections', () => {
+      //add section
+      //remove section
+    });
+  });
+  describe('Save', () => {
+    //save collection
+    //delete collection
+    //execute saves and deletes
   });
 });
+
+const render = async () => {
+  return await renderRootComponent(EditResumeComponent, {});
+};
