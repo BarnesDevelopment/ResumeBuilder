ecf8c8bfe3397faca99176e11b19e4ce4e255bc2
SamBobBarnes <samuelrbarnes@outlook.com>
2024-08-04 21:46:23 -0500

    education

--- App/resume-builder/src/app/resume/components/edit-resume/components/section-education/section-education.component.spec.ts
+++ App/resume-builder/src/app/resume/components/edit-resume/components/section-education/section-education.component.spec.ts
@@ -1,23 +1,128 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {
+  Guid,
+  NodeType,
+  renderRootComponent,
+  ResumeTreeNode,
+  screen,
+  fireEvent,
+} from '../../../../../common/testing-imports';
 import { SectionEducationComponent } from './section-education.component';
 
 describe('SectionEducationComponent', () => {
-  let component: SectionEducationComponent;
-  let fixture: ComponentFixture<SectionEducationComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [SectionEducationComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(SectionEducationComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  let node: ResumeTreeNode;
+  beforeEach(() => {
+    node = {
+      children: [],
+      nodeType: NodeType.Education,
+      id: Guid.create(),
+      parentId: Guid.create(),
+      content: '',
+      comments: '',
+      active: true,
+      depth: 0,
+      userId: Guid.create(),
+      order: 0,
+    };
   });
+  it('should create child nodes on creation', async () => {
+    const { fixture } = await render(node);
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+    fixture.componentInstance.node.children.forEach((child, index) => {
+      expect(child.nodeType).toBe(NodeType.ListItem);
+      expect(child.order).toBe(index);
+      expect(child.parentId).toBe(node.id);
+      expect(child.depth).toBe(node.depth + 1);
+      expect(child.content).toBe('');
+    });
+  });
+  it('should emit saves on creation', async () => {
+    const { fixture } = await render(node);
+    fixture.componentInstance.node.children = [];
+    const onSave = jest.spyOn(fixture.componentInstance.onSave, 'emit');
+
+    fixture.componentInstance.ngOnInit();
+
+    expect(onSave).toHaveBeenCalledTimes(7);
+  });
+  it('should update nodes on text updates', async () => {
+    const { fixture } = await render(node);
+
+    fireEvent.input(screen.getByTitle('degree'), {
+      target: { value: 'degree changed' },
+    });
+    fireEvent.input(screen.getByTitle('major'), {
+      target: { value: 'major changed' },
+    });
+    fireEvent.input(screen.getByTitle('minor'), {
+      target: { value: 'minor changed' },
+    });
+    fireEvent.input(screen.getByTitle('school'), {
+      target: { value: 'school changed' },
+    });
+    fireEvent.input(screen.getByTitle('city'), {
+      target: { value: 'city changed' },
+    });
+    fireEvent.input(screen.getByTitle('state'), {
+      target: { value: 'state changed' },
+    });
+    fireEvent.input(screen.getByTitle('graduationYear'), {
+      target: { value: '2244' },
+    });
+
+    expect(fixture.componentInstance.node.children[0].content).toBe(
+      'degree changed',
+    );
+    expect(fixture.componentInstance.node.children[1].content).toBe(
+      'major changed',
+    );
+    expect(fixture.componentInstance.node.children[2].content).toBe(
+      'minor changed',
+    );
+    expect(fixture.componentInstance.node.children[3].content).toBe(
+      'school changed',
+    );
+    expect(fixture.componentInstance.node.children[4].content).toBe(
+      'city changed',
+    );
+    expect(fixture.componentInstance.node.children[5].content).toBe(
+      'state changed',
+    );
+    expect(fixture.componentInstance.node.children[6].content).toBe('2244');
+  });
+  it('should emit saves on text updates', async () => {
+    const { fixture } = await render(node);
+    const onSave = jest.spyOn(fixture.componentInstance.onSave, 'emit');
+
+    fireEvent.input(screen.getByTitle('degree'), {
+      target: { value: 'degree changed' },
+    });
+    fireEvent.input(screen.getByTitle('major'), {
+      target: { value: 'major changed' },
+    });
+    fireEvent.input(screen.getByTitle('minor'), {
+      target: { value: 'minor changed' },
+    });
+    fireEvent.input(screen.getByTitle('school'), {
+      target: { value: 'school changed' },
+    });
+    fireEvent.input(screen.getByTitle('city'), {
+      target: { value: 'city changed' },
+    });
+    fireEvent.input(screen.getByTitle('state'), {
+      target: { value: 'state changed' },
+    });
+    fireEvent.input(screen.getByTitle('graduationYear'), {
+      target: { value: '2244' },
+    });
+
+    expect(onSave).toHaveBeenCalledTimes(7);
   });
 });
+
+const render = async (node: ResumeTreeNode) => {
+  return await renderRootComponent(SectionEducationComponent, {
+    componentProperties: {
+      node,
+    },
+  });
+};
--- App/resume-builder/src/app/resume/components/edit-resume/components/section-education/section-education.component.ts
+++ App/resume-builder/src/app/resume/components/edit-resume/components/section-education/section-education.component.ts
@@ -1,14 +1,81 @@
-import { Component, Input } from '@angular/core';
-import { ResumeTreeNode } from '../../../../../models/Resume';
+import { Component, Input, OnInit } from '@angular/core';
+import {
+  newResumeTreeNode,
+  NodeType,
+  ResumeTreeNode,
+} from '../../../../../models/Resume';
 import { UpsertSignal } from '../upsert-signal/upsert-signal';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { InputComponent } from '../../../../../common/input/input.component';
 
 @Component({
   selector: 'app-section-education',
   standalone: true,
-  imports: [],
+  imports: [InputComponent, ReactiveFormsModule],
   templateUrl: './section-education.component.html',
   styleUrl: './section-education.component.scss',
 })
-export class SectionEducationComponent extends UpsertSignal {
+export class SectionEducationComponent extends UpsertSignal implements OnInit {
   @Input() node: ResumeTreeNode;
+
+  form: FormGroup;
+
+  ngOnInit() {
+    if (!this.node.children.length) {
+      this.CreateChildren();
+      this.node.children.forEach(child => {
+        this.onSave.emit(child);
+      });
+    }
+    console.log(this.node);
+    this.form = new FormGroup({
+      degree: new FormControl(this.node.children[0].content),
+      major: new FormControl(this.node.children[1].content),
+      minor: new FormControl(this.node.children[2].content),
+      school: new FormControl(this.node.children[3].content),
+      city: new FormControl(this.node.children[4].content),
+      state: new FormControl(this.node.children[5].content),
+      graduationYear: new FormControl(this.node.children[6].content),
+    });
+    this.form.controls['degree'].valueChanges.subscribe(value => {
+      this.node.children[0].content = value;
+      this.onSave.emit(this.node.children[0]);
+    });
+    this.form.controls['major'].valueChanges.subscribe(value => {
+      this.node.children[1].content = value;
+      this.onSave.emit(this.node.children[1]);
+    });
+    this.form.controls['minor'].valueChanges.subscribe(value => {
+      this.node.children[2].content = value;
+      this.onSave.emit(this.node.children[2]);
+    });
+    this.form.controls['school'].valueChanges.subscribe(value => {
+      this.node.children[3].content = value;
+      this.onSave.emit(this.node.children[3]);
+    });
+    this.form.controls['city'].valueChanges.subscribe(value => {
+      this.node.children[4].content = value;
+      this.onSave.emit(this.node.children[4]);
+    });
+    this.form.controls['state'].valueChanges.subscribe(value => {
+      this.node.children[5].content = value;
+      this.onSave.emit(this.node.children[5]);
+    });
+    this.form.controls['graduationYear'].valueChanges.subscribe(value => {
+      this.node.children[6].content = value;
+      this.onSave.emit(this.node.children[6]);
+    });
+  }
+
+  CreateChildren() {
+    this.node.children = [
+      newResumeTreeNode(NodeType.ListItem, 0, this.node),
+      newResumeTreeNode(NodeType.ListItem, 1, this.node),
+      newResumeTreeNode(NodeType.ListItem, 2, this.node),
+      newResumeTreeNode(NodeType.ListItem, 3, this.node),
+      newResumeTreeNode(NodeType.ListItem, 4, this.node),
+      newResumeTreeNode(NodeType.ListItem, 5, this.node),
+      newResumeTreeNode(NodeType.ListItem, 6, this.node),
+    ];
+  }
 }
