714bc1bf899747cae4e8c406ec9f7e1697fbf9a5
SamBobBarnes <samuelrbarnes@outlook.com>
2024-08-18 20:01:02 -0500

    app component test

--- App/resume-builder/src/app/app.component.spec.ts
+++ App/resume-builder/src/app/app.component.spec.ts
@@ -1,29 +1,47 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { renderRootComponent } from './common/testing-imports';
+import { OAuthService } from 'angular-oauth2-oidc';
+import { authConfig } from './services/auth/models/auth-config';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  let configure, setup, load;
+  beforeEach(async () => {
+    configure = jest.spyOn(MockOAuthService.prototype, 'configure');
+    setup = jest.spyOn(
+      MockOAuthService.prototype,
+      'setupAutomaticSilentRefresh',
+    );
+    load = jest.spyOn(
+      MockOAuthService.prototype,
+      'loadDiscoveryDocumentAndTryLogin',
+    );
+    await renderRootComponent(AppComponent, {
+      providers: [{ provide: OAuthService, useClass: MockOAuthService }],
+    });
   });
 
-  it(`should have as title 'resume-builder'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('resume-builder');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('resume-builder app is running!');
+  it('should setup oauth', () => {
+    expect(configure).toBeCalledWith({
+      issuer: 'https://auth.barnes7619.com/realms/ResumeBuilder',
+      redirectUri: 'https://localhost:4200/',
+      postLogoutRedirectUri: 'https://localhost:4200/',
+      silentRefreshRedirectUri: 'https://localhost:4200/silent-callback',
+      clientId: 'resume-builder',
+      dummyClientSecret: 'uh01jhYZqvotfl2I9DbNkTEm0QLBNKEH',
+      scope: 'profile offline_access',
+      responseType: 'code',
+      showDebugInformation: true,
+      strictDiscoveryDocumentValidation: false,
+    });
+    expect(setup).toBeCalledTimes(1);
+    expect(load).toBeCalledTimes(1);
   });
 });
+
+class MockOAuthService {
+  configure() {}
+  setupAutomaticSilentRefresh() {}
+  loadDiscoveryDocumentAndTryLogin() {}
+}
