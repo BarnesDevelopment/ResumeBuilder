3c7d8b4ed718fd7acd42ede4ea2f39e1d9ff5f67
SamBobBarnes <samuelrbarnes@outlook.com>
2024-08-18 19:38:10 -0500

    interceptor test

--- App/resume-builder/src/interceptors/auth.interceptor.spec.ts
+++ App/resume-builder/src/interceptors/auth.interceptor.spec.ts
@@ -1,16 +1,48 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthInterceptor } from './auth.interceptor';
+import { OAuthService } from 'angular-oauth2-oidc';
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpRequest,
+  HttpResponse,
+} from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import mock = jest.mock;
 
 describe('AuthInterceptor', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    providers: [
-      AuthInterceptor
-      ]
-  }));
+  let request: HttpRequest<any> = new HttpRequest('GET', 'My.Domain.net');
+  let handler;
+  const mockHandler = {
+    handle: (req: HttpRequest<any>): Observable<HttpEvent<any>> =>
+      of(new HttpResponse(req)),
+  };
+  beforeEach(() => {
+    handler = jest.spyOn(mockHandler, 'handle');
+    return TestBed.configureTestingModule({
+      providers: [
+        AuthInterceptor,
+        {
+          provide: OAuthService,
+          useValue: {
+            getAccessToken: (): string => 'MyAccessToken',
+          },
+        },
+      ],
+    });
+  });
 
-  it('should be created', () => {
+  it('should add auth header', () => {
+    handler.mockImplementation(() => of({}));
     const interceptor: AuthInterceptor = TestBed.inject(AuthInterceptor);
-    expect(interceptor).toBeTruthy();
+    interceptor.intercept(request, mockHandler);
+
+    expect(handler).toHaveBeenCalledTimes(1);
+
+    const mock = handler.mock.calls[0][0];
+    expect(
+      mock.headers.lazyUpdate.filter(x => x.name == 'Authorization')[0].value,
+    ).toEqual('Bearer MyAccessToken');
   });
 });
