27d06f5ed9180282e5be49bd4e8b5fa587174162
sb17057 <sbarnes@vermeer.com>
2024-09-12 15:56:56 -0500

    updated traefik rule

--- App/resume-builder/src/environment/environment.production.ts
+++ App/resume-builder/src/environment/environment.production.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBasePath: 'https://resume-builder-api.barnes-development.com/api',
+  apiBasePath: 'https://resume-builder-api.barnes-development.com',
   domain: 'resume-builder.barnes-development.com',
   loggedIn: false,
 };
