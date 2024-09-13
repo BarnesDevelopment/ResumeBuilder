ee333842e8ea6aa6d945255b610db9819620b32f
sb17057 <sbarnes@vermeer.com>
2024-09-12 19:21:10 -0500

    fix uri

--- App/resume-builder/src/environment/environment.production.ts
+++ App/resume-builder/src/environment/environment.production.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBasePath: 'https://resume-builder-api.barnes-development.com',
+  apiBasePath: 'https://resume-builder.barnes-development.com',
   domain: 'resume-builder.barnes-development.com',
   loggedIn: false,
 };
