cf3ca4ef2db4109d6963430406f8f4d9f4674a63
SamBobBarnes <samuelrbarnes@outlook.com>
2024-09-20 22:33:59 -0500

    db layer user create, get, updatetime

--- ResumeAPI/ResumeAPI/Database/UserData.cs
+++ ResumeAPI/ResumeAPI/Database/UserData.cs
@@ -7,8 +7,11 @@ namespace ResumeAPI.Database;
 public interface IUserData
 {
   Task<User?> GetUser(Guid id);
+  Task<User?> GetUser(string cookie);
   Task<Guid> CreateUser(Guid id);
+  Task<string> CreateUser(Guid id, string cookie);
   Task<bool> DeleteUser(Guid id);
+  Task UpdateAccessedDate(Guid id);
 }
 
 public class UserData : PostgreSqlContext, IUserData
@@ -22,7 +25,16 @@ public class UserData : PostgreSqlContext, IUserData
     return (await Db.QueryAsync<User>($@"select 
                     id {nameof(User.Id)},
                     created_date {nameof(User.CreatedDate)}
-                    from ResumeDb.Users where id = @id", new { id = id }))
+                    from ResumeDb.Users where id = @id", new { id }))
+      .FirstOrDefault();
+  }
+
+  public async Task<User?> GetUser(string cookie)
+  {
+    return (await Db.QueryAsync<User>($@"select 
+                    id {nameof(User.Id)},
+                    created_date {nameof(User.CreatedDate)}
+                    from ResumeDb.Users where cookie = @cookie", new { cookie }))
       .FirstOrDefault();
   }
 
@@ -38,8 +50,26 @@ public class UserData : PostgreSqlContext, IUserData
     return id;
   }
 
+  public async Task<string> CreateUser(Guid id, string cookie)
+  {
+    await Db.ExecuteAsync(
+      @"insert into ResumeDb.Users (id, cookie) values(@id, @cookie)",
+      new
+      {
+        id,
+        cookie
+      });
+
+    return cookie;
+  }
+
   public async Task<bool> DeleteUser(Guid id)
   {
-    return await Db.ExecuteAsync("delete from ResumeDb.Users where id = @id", new { id = id }) > 0;
+    return await Db.ExecuteAsync("delete from ResumeDb.Users where id = @id", new { id }) > 0;
+  }
+
+  public async Task UpdateAccessedDate(Guid id)
+  {
+    await Db.ExecuteAsync("update ResumeDb.Users set last_accessed = now() where id = @id", new { id });
   }
 }
