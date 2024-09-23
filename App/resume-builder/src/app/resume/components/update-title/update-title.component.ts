9e879aa0517ff9f05cce2b6b2708db14b456d8b7
sb17057 <sbarnes@vermeer.com>
2024-09-23 10:21:44 -0500

    copy dialog opens

---
+++ App/resume-builder/src/app/resume/components/update-title/update-title.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-title',
+  standalone: true,
+  imports: [],
+  templateUrl: './update-title.component.html',
+  styleUrl: './update-title.component.scss',
+})
+export class UpdateTitleComponent {}
