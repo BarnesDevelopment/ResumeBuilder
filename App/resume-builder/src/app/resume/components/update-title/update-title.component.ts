a79070af245b99813e8b25f7d3318953432f3c6d
sb17057 <sbarnes@vermeer.com>
2024-09-24 16:35:32 -0500

    wip testing. have flaky tests

--- App/resume-builder/src/app/resume/components/update-title/update-title.component.ts
+++ App/resume-builder/src/app/resume/components/update-title/update-title.component.ts
@@ -4,7 +4,12 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Guid } from 'guid-typescript';
 import { ResumeTreeNode } from '../../../models/Resume';
 import { InputComponent } from '../../../common/input/input.component';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import {
   ButtonComponent,
   ButtonStyle,
@@ -27,9 +32,12 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 export class UpdateTitleComponent implements OnInit {
   private readonly service = inject(ResumeService);
   private readonly router = inject(Router);
+  protected readonly ButtonStyle = ButtonStyle;
   isLoading: boolean = true;
+  saveDisabled: boolean = true;
   resume: ResumeTreeNode;
   form: FormGroup;
+  titleError: string;
 
   constructor(
     public dialogRef: MatDialogRef<UpdateTitleComponent>,
@@ -44,31 +52,65 @@ export class UpdateTitleComponent implements OnInit {
     this.service.getResume(this.data.id.toString()).subscribe(resume => {
       this.resume = resume;
       this.form = new FormGroup({
-        title: new FormControl(this.resume.content),
+        title: new FormControl(this.resume.content, [
+          Validators.required,
+          Validators.pattern(/^(\s+\S+\s*)*(?!\s).*$/),
+        ]),
         comments: new FormControl(this.resume.comments),
+      });
+      this.form.valueChanges.subscribe(() => {
+        this.validateForm();
       });
       this.isLoading = false;
     });
   }
 
-  protected readonly ButtonStyle = ButtonStyle;
+  validateForm() {
+    const invalidForm = this.form.invalid;
+    const titleMatch =
+      this.form.get('title').value.trim() === this.resume.content.trim();
+
+    if (titleMatch) {
+      this.form.get('title').setErrors({ titleMatch: true });
+    }
+
+    if (
+      this.form.get('title').hasError('required') ||
+      this.form.get('title').hasError('pattern')
+    ) {
+      this.titleError = 'Title cannot be blank';
+    } else if (this.form.get('title').hasError('titleMatch')) {
+      this.titleError = 'Title must be different';
+    } else {
+      this.titleError = '';
+    }
+
+    this.form.markAllAsTouched();
+    this.form.updateValueAndValidity();
+
+    this.saveDisabled = invalidForm || titleMatch;
+  }
 
   save() {
+    this.validateForm();
+    if (this.saveDisabled) {
+      return;
+    }
     this.service.duplicateResume(this.data.id.toString()).subscribe(id => {
       this.service.getResume(id).subscribe(resume => {
         resume.content = this.form.get('title').value;
         resume.comments = this.form.get('comments').value;
         resume.order = this.data.next;
-        console.log({
-          content: resume.content,
-          comments: resume.comments,
-          order: resume.order,
-        });
         this.service.updateResume([resume]).subscribe(() => {
+          console.log('updated');
           this.router.navigate(['/edit', id]);
           this.dialogRef.close();
         });
       });
     });
+  }
+
+  cancel() {
+    this.dialogRef.close();
   }
 }
