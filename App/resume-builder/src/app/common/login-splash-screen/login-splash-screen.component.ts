5dc868311665171cd194d620ebd02c28960ea671
sb17057 <sbarnes@vermeer.com>
2024-11-04 16:26:48 -0600

    add demo button to login spash screen

--- App/resume-builder/src/app/common/login-splash-screen/login-splash-screen.component.ts
+++ App/resume-builder/src/app/common/login-splash-screen/login-splash-screen.component.ts
@@ -1,21 +1,29 @@
-import { Component } from '@angular/core';
-import { BorderStyle, ButtonStyle, ButtonComponent } from '../button/button.component';
+import { Component, inject } from '@angular/core';
+import {
+  BorderStyle,
+  ButtonComponent,
+  ButtonStyle,
+} from '../button/button.component';
 import { OAuthService } from 'angular-oauth2-oidc';
+import { Router } from '@angular/router';
 
 @Component({
-    selector: 'app-login-splash-screen',
-    templateUrl: './login-splash-screen.component.html',
-    styleUrls: ['./login-splash-screen.component.scss'],
-    standalone: true,
-    imports: [ButtonComponent],
+  selector: 'app-login-splash-screen',
+  templateUrl: './login-splash-screen.component.html',
+  styleUrls: ['./login-splash-screen.component.scss'],
+  standalone: true,
+  imports: [ButtonComponent],
 })
 export class LoginSplashScreenComponent {
   protected readonly ButtonStyle = ButtonStyle;
   protected readonly BorderStyle = BorderStyle;
+  private readonly router = inject(Router);
 
   constructor(private oauthService: OAuthService) {}
 
   login() {
     this.oauthService.initCodeFlow();
   }
+
+  demo() {}
 }
