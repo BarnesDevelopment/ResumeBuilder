f35a15261c8fe4a8e17f448b4098c00aec201b20
sb17057 <sbarnes@vermeer.com>
2024-11-04 12:25:33 -0600

    update routes
    Signed-off-by: sb17057 <sbarnes@vermeer.com>

--- ResumeAPI/ResumeAPI/Controllers/UserController.cs
+++ ResumeAPI/ResumeAPI/Controllers/UserController.cs
@@ -6,60 +6,60 @@ using ResumeAPI.Services;
 namespace ResumeAPI.Controllers;
 
 [ApiController]
-[Route("resume/users/")]
+[Route("resume/users")]
 public class UserController : ControllerBase
 {
-  private readonly ILogger<UserController> _logger;
-  private readonly IUserOrchestrator _orchestrator;
+    private readonly ILogger<UserController> _logger;
+    private readonly IUserOrchestrator _orchestrator;
 
-  public UserController(ILogger<UserController> logger, IUserService service, IUserOrchestrator orchestrator)
-  {
-    _logger = logger;
-    _orchestrator = orchestrator;
-  }
-
-  #region User
-
-  /// <summary>
-  /// Create user
-  /// </summary>
-  /// <returns></returns>
-  [HttpPost("create")]
-  [ProducesResponseType(typeof(User), 201)]
-  public async Task<IActionResult> CreateUser()
-  {
-    try
+    public UserController(ILogger<UserController> logger, IUserService service, IUserOrchestrator orchestrator)
     {
-      return Created("", await _orchestrator.CreateUser());
+        _logger = logger;
+        _orchestrator = orchestrator;
     }
-    catch (Exception e)
-    {
-      _logger.LogError(e.Message);
-      return Problem(e.Message);
-    }
-  }
 
-  /// <summary>
-  /// Delete User
-  /// </summary>
-  /// <param name="id">UserId</param>
-  /// <returns></returns>
-  [HttpDelete("{id}")]
-  [ProducesResponseType(202)]
-  [ProducesResponseType(404)]
-  public async Task<IActionResult> DeleteUser([FromRoute] string id)
-  {
-    try
-    {
-      if (await _orchestrator.DeleteUser(Guid.Parse(id))) return Accepted();
-      return NotFound();
-    }
-    catch (Exception e)
-    {
-      _logger.LogError(e.Message);
-      return Problem(e.Message);
-    }
-  }
+    #region User
 
-  #endregion
+    /// <summary>
+    /// Create user
+    /// </summary>
+    /// <returns></returns>
+    [HttpPost]
+    [ProducesResponseType(typeof(User), 201)]
+    public async Task<IActionResult> CreateUser()
+    {
+        try
+        {
+            return Created("", await _orchestrator.CreateUser());
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e.Message);
+            return Problem(e.Message);
+        }
+    }
+
+    /// <summary>
+    /// Delete User
+    /// </summary>
+    /// <param name="id">UserId</param>
+    /// <returns></returns>
+    [HttpDelete("{id}")]
+    [ProducesResponseType(202)]
+    [ProducesResponseType(404)]
+    public async Task<IActionResult> DeleteUser([FromRoute] string id)
+    {
+        try
+        {
+            if (await _orchestrator.DeleteUser(Guid.Parse(id))) return Accepted();
+            return NotFound();
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e.Message);
+            return Problem(e.Message);
+        }
+    }
+
+    #endregion
 }
