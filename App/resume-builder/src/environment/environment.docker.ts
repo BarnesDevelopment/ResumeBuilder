bd4ad1ac54eff355decde62b75f1fb894dd70044
sb17057 <sbarnes@vermeer.com>
2025-04-11 10:46:48 -0500

    update env and deps

--- App/resume-builder/src/environment/environment.docker.ts
+++ App/resume-builder/src/environment/environment.docker.ts
@@ -3,7 +3,7 @@ import { Environment } from './environment-model';
 export const environment: Environment = {
   production: false,
   demo: false,
-  apiBasePath: 'https://localhost:7211/resume',
+  apiBasePath: 'http://localhost:8080/resume',
   domain: 'localhost:4200',
   loggedIn: false,
 };
