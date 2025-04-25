730836fe1c86ab0045a34dbb231244786de91e0e
sb17057 <sbarnes@vermeer.com>
2025-04-25 09:43:15 -0500

    wip testing

--- App/resume-builder/src/interceptors/auth.interceptor.spec.ts
+++ App/resume-builder/src/interceptors/auth.interceptor.spec.ts
@@ -2,17 +2,11 @@ import { TestBed } from '@angular/core/testing';
 
 import { AuthInterceptor } from './auth.interceptor';
 import { OAuthService } from 'angular-oauth2-oidc';
-import {
-  HttpEvent,
-  HttpHandler,
-  HttpRequest,
-  HttpResponse,
-} from '@angular/common/http';
+import { HttpEvent, HttpRequest, HttpResponse } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import mock = jest.mock;
 
 describe('AuthInterceptor', () => {
-  let request: HttpRequest<any> = new HttpRequest('GET', 'My.Domain.net');
+  const request: HttpRequest<any> = new HttpRequest('GET', 'My.Domain.net');
   let handler;
   const mockHandler = {
     handle: (req: HttpRequest<any>): Observable<HttpEvent<any>> =>
@@ -41,6 +35,7 @@ describe('AuthInterceptor', () => {
     expect(handler).toHaveBeenCalledTimes(1);
 
     const lastCall = handler.mock.calls[0][0];
+    console.log({ headers: lastCall });
     expect(
       lastCall.headers.lazyUpdate.filter(x => x.name == 'Authorization')[0]
         .value,
