5bb41dc710c4facb2e21c6abc0c3134901bbf2ed
SamBobBarnes <samuelrbarnes@outlook.com>
2025-09-18 07:57:39 -0500

    portainer provider comments

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "FusionAuth/fusionauth"
       version = "1.2.0"
     }
+# 	portainer = {
+# 	  source = "portainer/portainer"
+# 	  version = "1.12.2"
+# 	}
   }
 }
 
@@ -12,3 +16,15 @@ provider "fusionauth" {
   host    = var.fusionauth_host
 }
 
+# provider "portainer" {
+#   endpoint = "https://portainer.example.com"
+#   
+#     # Option 1: API key authentication
+#     api_key  = "your-api-key"
+#   
+#     # Option 2: Username/password authentication (generates JWT token internally)
+#     # api_user     = "admin"
+#     # api_password = "your-password"
+#   
+#     skip_ssl_verify  = true # optional (default value is `false`)
+# }
