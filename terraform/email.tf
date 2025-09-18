7df86a7a0d562f0cf83e707b3f5f0279f270d674
SamBobBarnes <sbarnes@vermeer.com>
2025-09-18 11:26:15 -0500

    updates

--- terraform/email.tf
+++ terraform/email.tf
@@ -1,6 +1,7 @@
 locals {
   email_templates_path = "${path.module}/email_templates"
   noreply_address     = "noreply@auth.barnes7616.com"
+  default_from_name   = "Resume Builder Auth"
 }
 
 resource "fusionauth_email" "forgot-password" {
@@ -14,7 +15,7 @@ resource "fusionauth_email" "forgot-password" {
 
 resource "fusionauth_email" "setup-password" {
   name                  = "Setup Password"
-  default_from_name     = "Setup Password"
+  default_from_name     = local.default_from_name
   default_html_template = file("${path.email_templates_path}/Setup_Password.html.ftl")
   default_subject       = "Setup Password"
   default_text_template = file("${path.email_templates_path}/Setup_Password.txt.ftl")
