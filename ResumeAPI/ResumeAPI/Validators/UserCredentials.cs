e4a4db7f752970bcbca58bddb1fe66ccc9953e14
SamBobBarnes <samuelrbarnes@outlook.com>
2025-10-17 13:38:16 -0400

    validator tests

--- ResumeAPI/ResumeAPI/Validators/UserCredentials.cs
+++ ResumeAPI/ResumeAPI/Validators/UserCredentials.cs
@@ -1,14 +1,38 @@
+using System.Security.Claims;
 using FluentValidation;
+using ResumeAPI.Database;
+using ResumeAPI.Models;
 using ResumeAPI.Services;
 
 namespace ResumeAPI.Validators;
 
-public class UserCredentials : AbstractValidator<HttpContext>
+public class UserCredentials : AbstractValidator<(HttpContext httpContext, Guid? resourceId)>
 {
-    public UserCredentials(IAuthClient authClient)
+    public UserCredentials(IResumeTree resumeDb, IAuthClient authClient)
     {
-        RuleFor(context => context.Request)
-            .SetValidator(new RequestValidator(authClient));
+        ResumeTreeNode? resource = null;
+        RuleFor(context => context.httpContext.Request)
+            .SetValidator(new RequestValidator(authClient))
+            .DependentRules(() => RuleFor(context => context.resourceId)
+                .Cascade(CascadeMode.Stop)
+                .MustAsync(async (context, resourceId, _) =>
+                {
+                    if (resourceId == null) return true;
+
+                    resource = await resumeDb.GetNode(resourceId.Value);
+                    return resource != null;
+                })
+                .WithMessage("The specified resource does not exist.")
+                .Must((context, resourceId, _) =>
+                {
+                    if (resourceId == null) return true;
+
+                    var userId = Guid.Parse((context.httpContext.User.Identity as ClaimsIdentity)!.FindFirst("userId")!
+                        .Value);
+
+                    return resource!.UserId == userId;
+                })
+                .WithMessage("User does not have access to the specified resource."));
     }
 }
 
@@ -36,7 +60,7 @@ public class HeaderValidator : AbstractValidator<IHeaderDictionary>
             .MustAsync(async (headers, _) =>
             {
                 var token = headers["Authorization"].ToString().Substring("Bearer ".Length).Trim();
-                
+
                 var response = await authClient.AuthenticateJwt(token);
 
                 return response;
---
+++ ResumeAPI/UnitTests/Validators/UserCredentialsTests.cs
@@ -0,0 +1,140 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using ResumeAPI.Database;
+using ResumeAPI.Models;
+using ResumeAPI.Services;
+using ResumeAPI.Validators;
+
+namespace UnitTests.Validators;
+
+public class UserCredentialsTests
+{
+    private readonly Mock<IAuthClient> _authClient;
+    private readonly Mock<IResumeTree> _resumeDb;
+    private readonly UserCredentials _validator;
+
+    public UserCredentialsTests()
+    {
+        _resumeDb = new Mock<IResumeTree>();
+        _authClient = new Mock<IAuthClient>();
+        _validator = new UserCredentials(_resumeDb.Object, _authClient.Object);
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("00000000-0000-0000-0000-000000000000")]
+    public async Task ValidCredentials(string? resourceIdString)
+    {
+        var httpContext = new DefaultHttpContext();
+        httpContext.Request.Headers["Authorization"] = "Bearer valid_token";
+        Guid? resourceId = Guid.TryParse(resourceIdString, out var guid) ? guid : null;
+        httpContext.Request.Headers["Authorization"] = "Bearer valid_token";
+        httpContext.User
+            = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
+            {
+                new("userId", resourceId.ToString() ?? Guid.NewGuid().ToString())
+            }));
+
+        _authClient.Setup(a => a.AuthenticateJwt("valid_token")).ReturnsAsync(true);
+        if (resourceId.HasValue)
+            _resumeDb.Setup(r => r.GetNode(resourceId.Value))
+                .ReturnsAsync(new ResumeTreeNode { UserId = resourceId.Value });
+
+        var actual = await _validator.ValidateAsync((httpContext, resourceId));
+
+        actual.IsValid.Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task MissingAuthorizationHeader()
+    {
+        var httpContext = new DefaultHttpContext();
+        var resourceId = Guid.NewGuid();
+
+        var actual = await _validator.ValidateAsync((httpContext, resourceId));
+
+        actual.IsValid.Should().BeFalse();
+        actual.Errors.Should().ContainSingle(e => e.ErrorMessage == "Missing Authorization header.");
+    }
+
+    [Fact]
+    public async Task EmptyAuthorizationHeader()
+    {
+        var httpContext = new DefaultHttpContext();
+        httpContext.Request.Headers["Authorization"] = "";
+        var resourceId = Guid.NewGuid();
+
+        var actual = await _validator.ValidateAsync((httpContext, resourceId));
+
+        actual.IsValid.Should().BeFalse();
+        actual.Errors.Should().ContainSingle(e => e.ErrorMessage == "Authorization header cannot be empty.");
+    }
+
+    [Fact]
+    public async Task AuthorizationHeaderNotStartingWithBearer()
+    {
+        var httpContext = new DefaultHttpContext();
+        httpContext.Request.Headers["Authorization"] = "InvalidHeaderValue";
+        var resourceId = Guid.NewGuid();
+
+        var actual = await _validator.ValidateAsync((httpContext, resourceId));
+
+        actual.IsValid.Should().BeFalse();
+        actual.Errors.Should().ContainSingle(e => e.ErrorMessage == "Authorization header must start with 'Bearer '.");
+    }
+
+    [Fact]
+    public async Task InvalidToken()
+    {
+        var httpContext = new DefaultHttpContext();
+        httpContext.Request.Headers["Authorization"] = "Bearer invalid_token";
+        var resourceId = Guid.NewGuid();
+
+        _authClient.Setup(a => a.AuthenticateJwt("invalid_token")).ReturnsAsync(false);
+
+        var actual = await _validator.ValidateAsync((httpContext, resourceId));
+
+        actual.IsValid.Should().BeFalse();
+        actual.Errors.Should().ContainSingle(e => e.ErrorMessage == "Invalid Authorization header.");
+    }
+
+    // resource id is provided but not found in database
+    // resource id is provided but does not belong to user
+
+    [Fact]
+    public async Task ResourceNotFound()
+    {
+        var httpContext = new DefaultHttpContext();
+        httpContext.Request.Headers["Authorization"] = "Bearer valid_token";
+        var resourceId = Guid.NewGuid();
+
+        _authClient.Setup(a => a.AuthenticateJwt("valid_token")).ReturnsAsync(true);
+        _resumeDb.Setup(r => r.GetNode(resourceId)).ReturnsAsync((ResumeTreeNode?)null);
+
+        var actual = await _validator.ValidateAsync((httpContext, resourceId));
+
+        actual.IsValid.Should().BeFalse();
+        actual.Errors.Should()
+            .ContainSingle(e => e.ErrorMessage == "The specified resource does not exist.");
+    }
+
+    [Fact]
+    public async Task ResourceDoesNotBelongToUser()
+    {
+        var httpContext = new DefaultHttpContext();
+        var resourceId = Guid.NewGuid();
+        httpContext.Request.Headers["Authorization"] = "Bearer valid_token";
+        httpContext.User
+            = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim> { new("userId", resourceId.ToString()) }));
+
+        _authClient.Setup(a => a.AuthenticateJwt("valid_token")).ReturnsAsync(true);
+        _resumeDb.Setup(r => r.GetNode(resourceId)).ReturnsAsync(new ResumeTreeNode { UserId = Guid.NewGuid() });
+
+        var actual = await _validator.ValidateAsync((httpContext, resourceId));
+
+        actual.IsValid.Should().BeFalse();
+        actual.Errors.Should()
+            .ContainSingle(e => e.ErrorMessage == "User does not have access to the specified resource.");
+    }
+}
