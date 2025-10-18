fda452696ac6aee1bef09ed134c61959aff93f71
SamBobBarnes <samuelrbarnes@outlook.com>
2025-10-18 10:51:07 -0400

    udpate access key

--- terraform/forum.tf
+++ terraform/forum.tf
@@ -64,6 +64,7 @@ resource "fusionauth_tenant" "resume-builder" {
   jwt_configuration {
     refresh_token_time_to_live_in_minutes = 43200
     time_to_live_in_seconds               = 3600
+    access_token_key_id                   = fusionauth_key.resume-builder-access-token.id
   }
   email_configuration {
     host                              = var.fusionauth_email_configuration_host
